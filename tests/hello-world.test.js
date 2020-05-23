fa82f2a1d665c3a51641a8b4e9a8cea7e5a8e8d3
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-05-22 23:15:34 -0300

    /hello-world: Added more meaningful HTML markup and the page title

--- tests/hello-world.test.js
+++ tests/hello-world.test.js
@@ -2,5 +2,6 @@ describe('/hello-world', () => {
   it('should render', async () => {
     await page.goto(`${APP_HOST}/hello-world`)
     await expect(page).toMatch(/first web page/i)
+    expect(await page.title()).toMatch(/first web page/i)
   })
 })
