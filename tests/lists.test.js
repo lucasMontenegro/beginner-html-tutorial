04beb72fec118a47a16c496884c53f893dadac68
lucasMontenegro <40044087+lucasMontenegro@users.noreply.github.com>
2020-05-23 18:12:54 -0300

    /lists: Added nested lists

--- tests/lists.test.js
+++ tests/lists.test.js
@@ -2,7 +2,8 @@ describe('/lists', () => {
   it('should render', async () => {
     await page.goto(`${APP_HOST}/lists`)
     await expect(page).toMatch(/html lists/i)
-    await expect(page).toMatchElement('ul', { visible: true, text: /.+/ })
+    await expect(page).toMatchElement('ol', { visible: true, text: /.+/ })
+    await expect(page).toMatchElement('ol ul', { visible: true, text: /.+/ })
     await expect(page).toMatchElement('li', { visible: true, text: /.+/ })
   })
 })
